296ccc6240ba438daedf3433983b3290b4a3e7fd
freestyle <annemarie@amparsons.co.uk>
2014-04-22 13:08:14 +0100

    form fixes

--- wp-content/themes/iooa/404.php
+++ wp-content/themes/iooa/404.php
@@ -1,22 +1,26 @@
 <?php get_header(); ?>
 
-	<section id="main" role="main">
-
-		<article id="post-0" class="post error404 not-found">
-
-			<h1>Oops! That page can&rsquo;t be found.</h1>
-
-			<div class="entry">
-
-				<p>It looks like nothing was found at this location. Maybe try heading back to the <a href="<?php echo home_url('/'); ?>">home page</a> or a search?</p>
-
-				<?php get_search_form(); ?>
-
-			</div>
-
-		</article>
-
-	</section> <!-- /#main -->
+	<main id="main" role="main">
+		<div class="wrapper">
+        	<div class="sharethisbar"><?php get_template_part( 'templates/partials/inc-socialbuttons'); ?></div>
+            <section>
+                <div class="head">
+                	<h1>Oops! That page can&rsquo;t be found.</h1>
+                 </div>
+                <p>It looks like nothing was found at this location. Maybe try heading back to the <a href="<?php echo home_url('/'); ?>">home page</a>.</p>
+            </section>
+            
+            <aside>
+				<?php get_template_part( 'templates/partials/aside', 'howto' ); ?>
+                
+                <?php get_template_part( 'templates/partials/aside', 'sponsors' ); ?>
+                
+                <?php get_template_part( 'templates/partials/aside', 'night-of-the-awards' ); ?>
+                
+                <?php get_template_part( 'templates/partials/aside', 'gallery' ); ?>
+            </aside>
+		</div>
+	</main> <!-- /#main -->
 
 <?php get_sidebar(); ?>
 
